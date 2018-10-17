4be8d206644f7b649b590c7b404ea7a2910fc82b
Abbie L <abbielaclaire@gmail.com>
2018-10-17 11:56:24 -0700

    Acme Create

---
+++ Acme/Acme/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Acme
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome, " + name + " to Acme Accounting Systems!");
+            Console.WriteLine("Remember, we're 'Accounting' on you!");
+            Console.Read();
+        }
+    }
+}
