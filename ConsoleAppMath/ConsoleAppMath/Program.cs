f110327399560f9d730443b1d4c7e2c54a444f63
Abbie L <abbielaclaire@gmail.com>
2018-11-10 17:02:03 -0500

    Math_step66

---
+++ ConsoleAppMath/ConsoleAppMath/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a whole number.");
+            long input = long.Parse(Console.ReadLine());
+            long product = input * 50;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick a whole number.");
+            long num1 = long.Parse(Console.ReadLine());
+            long total = num1 + 25;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Pick a number");
+            double num2 = double.Parse(Console.ReadLine());
+            double quotient = num2 / 12.5;
+            Console.WriteLine(quotient);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Pick a number");
+            double num3 = double.Parse(Console.ReadLine());
+            int comNum = 50;
+            bool number = num3 > comNum;
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+            Console.WriteLine("Pick a number");
+            double num4 = double.Parse(Console.ReadLine());
+            double remainder = num4 % 2;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Math/Math/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + total.ToString()));
+            //Console.ReadLine();
+
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine("Ten times Five= " + product.ToString());
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine("One Hundred divided Five = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //int quotient = 100 / 7;
+            //Console.WriteLine("One Hundred divided by 7 = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine("One Hundred divided by seventeen = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 4 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTeperature = 70;
+            //int currentTemperature = 72;
+            //bool isWarm = currentTemperature > roomTeperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //int roomTeperature = 70;
+            //int currentTemperature = 70;
+            //bool isWarm = currentTemperature >= roomTeperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            int roomTeperature = 70;
+            int currentTemperature = 70;
+            bool isWarm = currentTemperature == roomTeperature; //! is a NOT, replace the = with ! to reverse the answer is NOT equal
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+    
+    
+}
