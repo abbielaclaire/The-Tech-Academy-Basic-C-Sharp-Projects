d443b6718240e1253b3435fce4d8b3f0dc249052
Abbie L <abbielaclaire@gmail.com>
2018-11-10 08:53:54 -0500

    New Program

---
+++ DailyInquiryStudent/DailyInquiryStudent/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyInquiryStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            //student's cours and page
+            Console.WriteLine("What Course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number or step number are you on?");
+            string pageNumber = Console.ReadLine();
+
+
+            //need help
+            Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'");
+            string help = Console.ReadLine();
+
+            bool val = Convert.ToBoolean(help);
+
+            if (val == true)
+            {
+                Console.WriteLine("An Instructor will contact you soon.");
+            }
+
+            else
+            {
+                Console.WriteLine("Keep up the great work!");
+            }
+            Console.ReadLine();
+
+
+            //Study hours
+            Console.WriteLine("How many hours did you study today?");
+            string study = Console.ReadLine();
+            string hours = study.ToString();
+
+            //Experiences
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be speific.");
+            string feedback = Console.ReadLine();
+
+            //Close
+            Console.WriteLine("Thank you for your answers. An instructor will responf to this shortly. Have a Great Day!");
+            Console.ReadLine();
+
+
+        }
+        }
+
+
+
+
+    }
+
---
+++ VariablesandDatatypes/VariablesandDatatypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesandDatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
+            Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            //Casting from int to string
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum + 5;
+            Console.WriteLine("Your favorite number plus 5 is: " + total);
+            Console.ReadLine();
+
+            //Data Types
+            bool isStudying = false; //True or False
+            byte hoursWorked = 42; //0-255
+            sbyte currentTemperature = -32; //negative numbers
+            char questionMark = '\u2103';
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+            decimal moneyInBank = 100.5m;//used only for finacial transactions
+            double heightInCentimeters = 211.30202092;
+            float secondsLeft = 2.62f;
+            short temperatureOnMars = -341;
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
