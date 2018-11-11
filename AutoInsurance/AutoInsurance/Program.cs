700240bd0469525cf001636d29c802e6eb45b836
Abbie L <abbielaclaire@gmail.com>
2018-11-11 16:06:43 -0500

    Auto Insuracne Program

---
+++ AutoInsurance/AutoInsurance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looking for Auto Insurance? Answer some questions to see if you qualify:");
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Yes or No?");
+            string drunk = Console.ReadLine();
+           
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tix = int.Parse(Console.ReadLine());
+
+            bool qualify = ((age > 15) && (tix < 3) && (drunk == "No"));
+
+            Console.WriteLine(qualify);
+                                  
+            if (qualify == true)
+            {
+                Console.WriteLine("Congratulations!! You can get Auto Insurance.");
+            }
+
+            else
+            {
+                Console.WriteLine("Better Luck next Time!");
+            }
+            Console.ReadLine();
+
+
+            
+
+           
+
+        }
+    }
+}
--- Math/Math/Program.cs
+++ Math/Math/Program.cs
@@ -10,69 +10,101 @@ namespace Math
     {
         static void Main(string[] args)
         {
-            int total = 5 + 10;
-            Console.WriteLine("Five plus Ten = " + total.ToString()));
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + total.ToString()));
+            //Console.ReadLine();
+
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine("Ten times Five= " + product.ToString());
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine("One Hundred divided Five = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //int quotient = 100 / 7;
+            //Console.WriteLine("One Hundred divided by 7 = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine("One Hundred divided by seventeen = " + quotient.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 4 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTeperature = 70;
+            //int currentTemperature = 72;
+            //bool isWarm = currentTemperature > roomTeperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //int roomTeperature = 70;
+            //int currentTemperature = 70;
+            //bool isWarm = currentTemperature >= roomTeperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //int roomTeperature = 70;
+            //int currentTemperature = 70;
+            //bool isWarm = currentTemperature == roomTeperature; //! is a NOT, replace the = with ! to reverse the answer is NOT equal
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //BooleanLogic
+
+            ////the 'AND'
+            //Console.WriteLine(true && false); //&& is 'and'
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            ////the 'OR'
+            //Console.WriteLine(true || true); // || is 'or' symbol is above enter
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            ////the 'EQUAL'
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            ////the 'NOT'
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+
+            ////will go to True if one is true 'X OR'
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.WriteLine(true && true && true || false);
             Console.ReadLine();
 
-            int total = 5 + 10;
-            int otherTotal = 12 + 22;
-            int combined = total + otherTotal;
-            Console.WriteLine(combined);
-            Console.ReadLine();
-
-            int difference = 10 - 5;
-            Console.WriteLine("Ten minus Five = " + difference.ToString());
-            Console.ReadLine();
-
-            int product = 10 * 5;
-            Console.WriteLine("Ten times Five= " + product.ToString());
-            Console.ReadLine();
-
-            int quotient = 100 / 5;
-            Console.WriteLine("One Hundred divided Five = " + quotient.ToString());
-            Console.ReadLine();
-
-            int quotient = 100 / 7;
-            Console.WriteLine("One Hundred divided by 7 = " + quotient.ToString());
-            Console.ReadLine();
-
-            double quotient = 100.0 / 17.0;
-            Console.WriteLine("One Hundred divided by seventeen = " + quotient.ToString());
-            Console.ReadLine();
-
-            int remainder = 10 % 2;
-            Console.WriteLine(remainder);
-            Console.ReadLine();
-
-            int remainder = 11 % 2;
-            Console.WriteLine(remainder);
-            Console.ReadLine();
-
-            bool trueOrFalse = 12 > 5;
-            Console.WriteLine(trueOrFalse.ToString());
-            Console.ReadLine();
-
-            bool trueOrFalse = 4 > 5;
-            Console.WriteLine(trueOrFalse.ToString());
-            Console.ReadLine();
-
-            int roomTeperature = 70;
-            int currentTemperature = 72;
-            bool isWarm = currentTemperature > roomTeperature;
-            Console.WriteLine(isWarm);
-            Console.ReadLine();
-
-            int roomTeperature = 70;
-            int currentTemperature = 70;
-            bool isWarm = currentTemperature >= roomTeperature;
-            Console.WriteLine(isWarm);
-            Console.ReadLine();
-
-            int roomTeperature = 70;
-            int currentTemperature = 70;
-            bool isWarm = currentTemperature == roomTeperature; //! is a NOT, replace the = with ! to reverse the answer is NOT equal
-            Console.WriteLine(isWarm);
-            Console.ReadLine();
         }
     }
     
