afbe26423d1e4a4920556e3ef1081e686e02042e
Abbie L <abbielaclaire@gmail.com>
2018-11-11 11:36:53 -0500

    SalaryComp Program

--- Math/Math/Program.cs
+++ Math/Math/Program.cs
@@ -10,63 +10,63 @@ namespace Math
     {
         static void Main(string[] args)
         {
-            //int total = 5 + 10;
-            //Console.WriteLine("Five plus Ten = " + total.ToString()));
-            //Console.ReadLine();
+            int total = 5 + 10;
+            Console.WriteLine("Five plus Ten = " + total.ToString()));
+            Console.ReadLine();
 
-            //int total = 5 + 10;
-            //int otherTotal = 12 + 22;
-            //int combined = total + otherTotal;
-            //Console.WriteLine(combined);
-            //Console.ReadLine();
+            int total = 5 + 10;
+            int otherTotal = 12 + 22;
+            int combined = total + otherTotal;
+            Console.WriteLine(combined);
+            Console.ReadLine();
 
-            //int difference = 10 - 5;
-            //Console.WriteLine("Ten minus Five = " + difference.ToString());
-            //Console.ReadLine();
+            int difference = 10 - 5;
+            Console.WriteLine("Ten minus Five = " + difference.ToString());
+            Console.ReadLine();
 
-            //int product = 10 * 5;
-            //Console.WriteLine("Ten times Five= " + product.ToString());
-            //Console.ReadLine();
+            int product = 10 * 5;
+            Console.WriteLine("Ten times Five= " + product.ToString());
+            Console.ReadLine();
 
-            //int quotient = 100 / 5;
-            //Console.WriteLine("One Hundred divided Five = " + quotient.ToString());
-            //Console.ReadLine();
+            int quotient = 100 / 5;
+            Console.WriteLine("One Hundred divided Five = " + quotient.ToString());
+            Console.ReadLine();
 
-            //int quotient = 100 / 7;
-            //Console.WriteLine("One Hundred divided by 7 = " + quotient.ToString());
-            //Console.ReadLine();
+            int quotient = 100 / 7;
+            Console.WriteLine("One Hundred divided by 7 = " + quotient.ToString());
+            Console.ReadLine();
 
-            //double quotient = 100.0 / 17.0;
-            //Console.WriteLine("One Hundred divided by seventeen = " + quotient.ToString());
-            //Console.ReadLine();
+            double quotient = 100.0 / 17.0;
+            Console.WriteLine("One Hundred divided by seventeen = " + quotient.ToString());
+            Console.ReadLine();
 
-            //int remainder = 10 % 2;
-            //Console.WriteLine(remainder);
-            //Console.ReadLine();
+            int remainder = 10 % 2;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
 
-            //int remainder = 11 % 2;
-            //Console.WriteLine(remainder);
-            //Console.ReadLine();
+            int remainder = 11 % 2;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
 
-            //bool trueOrFalse = 12 > 5;
-            //Console.WriteLine(trueOrFalse.ToString());
-            //Console.ReadLine();
+            bool trueOrFalse = 12 > 5;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
 
-            //bool trueOrFalse = 4 > 5;
-            //Console.WriteLine(trueOrFalse.ToString());
-            //Console.ReadLine();
+            bool trueOrFalse = 4 > 5;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
 
-            //int roomTeperature = 70;
-            //int currentTemperature = 72;
-            //bool isWarm = currentTemperature > roomTeperature;
-            //Console.WriteLine(isWarm);
-            //Console.ReadLine();
+            int roomTeperature = 70;
+            int currentTemperature = 72;
+            bool isWarm = currentTemperature > roomTeperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
 
-            //int roomTeperature = 70;
-            //int currentTemperature = 70;
-            //bool isWarm = currentTemperature >= roomTeperature;
-            //Console.WriteLine(isWarm);
-            //Console.ReadLine();
+            int roomTeperature = 70;
+            int currentTemperature = 70;
+            bool isWarm = currentTemperature >= roomTeperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
 
             int roomTeperature = 70;
             int currentTemperature = 70;
---
+++ SalaryComparison/SalaryComparison/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1, Please enter Hourly Rate? Numbers only no dollar signs.");
+            decimal rate1 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Person 1, Please enter Hours worked per week?");
+            decimal worked1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Person 2, Please enter Hourly Rate? Numbers only no dollar signs.");
+            decimal rate2 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Person 1, Please enter Hours worked per week?");
+            decimal worked2 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Annual Salary of Person 1:");
+            decimal annual1 = rate1 * worked1;
+            string money1 = String.Format("{0:C}", annual1);
+            Console.WriteLine(money1);
+            Console.ReadLine();
+
+            Console.WriteLine("Annual Salary of Person 2:");
+            decimal annual2 = rate2 * worked2;
+            string money2 = String.Format("{0:C}", annual2);
+            Console.WriteLine(money2);
+            Console.ReadLine();
+
+            Console.WriteLine("Does Person 1 make more money thatn Person 2?");
+            decimal per1 = rate1 * worked1;
+            decimal per2 = rate2 * worked2;
+            bool isMore = per1 > per2;
+            Console.WriteLine(isMore);
+            Console.ReadLine();
+
+
+         
+
+        }
+    }
+}
