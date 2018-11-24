4a49a26e1afa265d5eed04b2b321322422a7531c
Abbie L <abbielaclaire@gmail.com>
2018-11-24 12:54:58 -0500

    Step 97 Strings

---
+++ EscapeCharacter/EscapeCharacter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The mane said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a horizontal tab.";
+            //string fileName = @"C:\User\Abbie";
+
+            //bool trueOrFalse = name.Contains("s");
+            //bool trueOrfalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+            //name = name.ToLower();
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            //string name = "Jesse";
+            //name = "Joe";
+
+            ////string builder- dynamic builder- it is expandable
+
+            //StringBuilder sb = new StringBuilder();
+
+            //sb.Append("My name is Jesse");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ MyStrings/MyStrings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStrings
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your job?");
+            string job = Console.ReadLine();
+            Console.WriteLine("What state do you live in?");
+            string state = Console.ReadLine();
+            state = state.ToUpper();
+            string[] combo = new String[] {$"Hi {name}, {job} is a nice job, you live in {state}"};
+            string combo1 = String.Join(",", combo);
+            Console.WriteLine(combo1);
+
+            Console.WriteLine("Let's continue...\n Name three friend's names.");
+            string name2 = Console.ReadLine();
+            name2 = name2.ToUpper();
+            Console.WriteLine($"Let's yell for your friends {name2}! They did not answer us!");
+            Console.ReadLine();
+            Console.WriteLine("Let's see if we can find them....");
+
+            StringBuilder sb = new StringBuilder();
+            
+            sb.Append($"Mighty Warrior {name}! \n");
+            Console.ReadLine();
+            sb.Append($"{name2} have just been arrested.");
+            sb.Append($"\n Your position as a {job} makes you the perfect person to help out {name2}.");
+            sb.Append($"\n your state {state} will allow you to represent them in court.");
+            sb.Append("\n Let's get down to business.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
