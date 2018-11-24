e37d43d05a87754f49b400cd09e4463e1fbd0396
Abbie L <abbielaclaire@gmail.com>
2018-11-24 13:13:49 -0500

    practice

---
+++ arraysList/arraysList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace arraysList
+{
+    class Program
+    {
+        static void Main()
+        {
+            ////Arrays do not change size
+
+            //btye[] byteArray = new byte[imagefile];
+
+            //List<string> intList = new List<string>();
+            //intList.Add("Hello");
+            //intList.Add("Jesse");
+            //intList.Add("Byte Me!");
+            
+            //Console.WriteLine(intList[1]);
+            //Console.ReadLine();
+                       
+            //List<int> intList = new List<int>();
+            //intList.Add(4);
+            //intList.Add(10);
+            //intList.Add(20);
+            //intList.Remove(20);
+            //Console.WriteLine(intList[1]);
+            //Console.ReadLine();
+
+            ////a number in the [] is the limit of the array
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //int[] numArray1 = new int[] { 2, 10, 100, 2000, 250 };
+
+            //int[] numArray2 = { 2, 10, 12, 200, 2098, 5000, 3857 };
+
+            //numArray2[5] = 650;
+
+            //Console.WriteLine(numArray2[5]);
+            //Console.ReadLine();
+
+        }
+    }
+}
