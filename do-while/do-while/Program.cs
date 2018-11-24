80b4ed2a30871bbba7249b6d9f5fbbed228a9187
Abbie L <abbielaclaire@gmail.com>
2018-11-24 11:21:54 -0500

    Do-While Loop step 94

---
+++ do-while/do-while/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite wild animal? ");
+            string animal = Console.ReadLine();
+            bool isGuessed = animal == "wolf";
+
+            do
+            {
+                switch (animal)
+                {
+                    case "bobcat":
+                        Console.WriteLine("You guessed bobcat, try again.");
+                        Console.WriteLine("Guess my favorite animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "fox":
+                        Console.WriteLine("You guessed fox, try again.");
+                        Console.WriteLine("Guess my favorite wild animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "dolpin":
+                        Console.WriteLine("You guessed dolphin, try again.");
+                        Console.WriteLine("Guess my favorite wild animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("You guessed incorrect, try again.");
+                        Console.WriteLine("Guess my favorite wild animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "wolf":
+                        Console.WriteLine("You guessed wolf, you are correct.");
+                        isGuessed = true;
+                        Console.ReadLine();
+                        break;
+                    case "Wolf":
+                        Console.WriteLine("You guessed wolf, you are correct.");
+                        isGuessed = true;
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.WriteLine("Lets do some math!! Pick a number between 1-100:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+
+            while (number < 1000)
+            {
+                
+                number = number + 1;
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("We have reached my favorite number 1000");
+            Console.ReadLine();
+
+        }
+    }
+}
--- loops/loops/Program.cs
+++ loops/loops/Program.cs
@@ -12,8 +12,72 @@ namespace loops
         {
             Console.WriteLine("Guess a number?");
             int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
 
-            switch (number)
+            do
+            {
+                switch (number)
+                {
+                    case 60:
+                        Console.WriteLine("You guessed 60, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 30:
+                        Console.WriteLine("You guessed 30, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("You guessed incorrect, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12, you are correct.");
+                        isGuessed = true;
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed); //the "!" is the same as writing isGuessed ==False
+            {
+                ////this is a while loop 
+                ////
+                //switch (number)
+                //{
+                //    case 60:
+                //        Console.WriteLine("You guessed 60, try again.");
+                //        Console.WriteLine("Guess a number?");
+                //        number = Convert.ToInt32(Console.ReadLine());
+                //        break;
+                //    case 30:
+                //        Console.WriteLine("You guessed 30, try again.");
+                //        Console.WriteLine("Guess a number?");
+                //        number = Convert.ToInt32(Console.ReadLine());
+                //        break;
+                //    case 55:
+                //        Console.WriteLine("You guessed 55, try again.");
+                //        Console.WriteLine("Guess a number?");
+                //        number = Convert.ToInt32(Console.ReadLine());
+                //        break;
+                //    default:
+                //        Console.WriteLine("You guessed incorrect, try again.");
+                //        Console.WriteLine("Guess a number?");
+                //        number = Convert.ToInt32(Console.ReadLine());
+                //        break;
+                //    case 12:
+                //        Console.WriteLine("You guessed 12, you are correct.");
+                //        isGuessed = true;
+                //        Console.ReadLine();
+                //        break;
+                //}
+            }
         }
     }
 }
